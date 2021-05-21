2514e4892181d847b1e878d4d9cf2e409a3a3c66
Edgar Eldy <ed.eldy21@live.com>
2021-05-21 23:04:10 +0200

    Get categories/index page with data

---
+++ routes/categories.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../models");
+const Category = db.Category;
+
+//Get categories/index page with data
+router.get("/categories", async function (req, res, next) {
+  Category.findAll().then((data) => {
+    res.render("categories/index", {
+      categories: data,
+    });
+  });
+});
