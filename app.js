1ed259d0224be9e0ac924cbc52a14c889736f6ea
Edgar Eldy <ed.eldy21@gmail.com>
2021-05-22 23:28:43 +0200

    Registered categories/index route

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// categories routes
+app.get('/categories', categoriesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
