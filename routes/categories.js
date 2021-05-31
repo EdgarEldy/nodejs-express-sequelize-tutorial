3c82abebc829e9bc2036a1f7fe704a2b2a07ab2f
Edgar Eldy <ed.eldy21@live.com>
2021-05-31 22:50:43 +0200

    Add a function for inserting a new product category

--- routes/categories.js
+++ routes/categories.js
@@ -5,16 +5,45 @@ const Category = db.Category;
 
 //Get categories/index page with data
 router.get("/categories", async function (req, res, next) {
-    Category.findAll().then((data) => {
-        res.render("categories/index", {
-            categories: data,
-        });
+  Category.findAll().then((data) => {
+    res.render("categories/index", {
+      categories: data,
     });
+  });
 });
 
 //Get categories/add view
-router.get('/categories/add', async function (req, res, next) {
-    res.render('categories/add');
+router.get("/categories/add", async function (req, res, next) {
+  res.render("categories/add");
+});
+
+// Add a new category
+router.post("/categories", async function (req, res, next) {
+  // Check errors
+  if (!req.body.category_name) {
+    res.render("categories/add", {
+      type: "danger",
+      message: "Category name can not be empty !",
+    });
+    return;
+  }
+
+  // Get category name from input
+  const category = {
+    category_name: req.body.category_name,
+  };
+
+  // Save a category
+  Category.create(category)
+    .then((data) => {
+      res.redirect("categories");
+    })
+    .catch((err) => {
+      res.render("categories/add", {
+        type: "danger",
+        message: err.message || "Error has been occured. Please try again.",
+      });
+    });
 });
 
 //Export categories routes
