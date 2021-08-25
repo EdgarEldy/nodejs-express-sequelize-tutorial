56522935a4961c0116151d88206edd9568dccbf2
Edgar Eldy <ed.eldy21@live.com>
2021-08-25 19:39:19 +0200

    Initialize connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var session = require('express-session');
+var flash = require('connect-flash');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
