ef750789eb2e6f17c9114a52911cf04816fa5fd2
Edgar Eldy <ed.eldy21@live.com>
2021-09-08 22:09:13 +0200

    Initialize customer model

--- routes/customers.js
+++ routes/customers.js
@@ -4,5 +4,7 @@ var router = expres.Router();
 // Initialize database connection
 const db = require('../models');
 
+// Initialize Customer model
+const Customer = db.Customer;
 
 module.exports = router;
