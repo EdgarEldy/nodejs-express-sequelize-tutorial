4523239fcda7608ab6bbab516e48aa5c9936febc
Edgar Eldy <ed.eldy21@live.com>
2021-09-08 22:13:13 +0200

    Get customers/index page with data

--- routes/customers.js
+++ routes/customers.js
@@ -4,6 +4,16 @@ var router = expres.Router();
 // Initialize database connection
 const db = require('../models');
 
+// Get customers/index page with data
+router.get("/customers", async function (req, res, next) {
+  Customer.findAll().then((data) => {
+    res.render("customers/index", {
+      customers: data,
+    });
+  });
+});
+
+
 // Initialize Customer model
 const Customer = db.Customer;
 
