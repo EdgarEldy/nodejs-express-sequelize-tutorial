7f7676c694f8c6aa9488b31210950842596153af
Edgar Eldy <ed.eldy21@live.com>
2021-10-19 20:35:10 +0200

    Add products data for testing

--- seeders/20211013210720-ProductSeeder.js
+++ seeders/20211013210720-ProductSeeder.js
@@ -11,6 +11,36 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    return queryInterface.bulkInsert("products", [
+      {
+        category_id: 1,
+        product_name: "Amstel",
+        unit_price: 2000,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        category_id: 1,
+        product_name: "Primus",
+        unit_price: 1600,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        category_id: 2,
+        product_name: "Citron",
+        unit_price: 800,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        category_id: 2,
+        product_name: "Coca cola",
+        unit_price: 800,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
