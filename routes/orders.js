6feebb9f9143fab4f9daba4b38686edf027b117c
Edgar Eldy <ed.eldy21@live.com>
2022-02-09 23:36:49 +0200

    Initialize Customer and Product models

--- routes/orders.js
+++ routes/orders.js
@@ -2,5 +2,8 @@ var express = require("express");
 var router = express.Router();
 const db = require("../models");
 
+const Customer = db.Customer;
+const Product = db.Product;
+
 // Initialize Order Model
 const Order = db.Order;
