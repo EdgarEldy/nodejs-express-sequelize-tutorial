8c9ad2880a66e18af9e8267ed810b185da4b51a8
Abikwa <Abikalex8@gmail.com>
2022-03-26 18:06:53 +0200

    add category associate product to test json request

--- models/category.js
+++ models/category.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Category.hasMany(models.Product, {
+        as: 'Invoices',
+        foreignKey: 'category_id',
+      });
     }
   }
   Category.init(
