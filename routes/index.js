f78934c72394803598b5f097f2bba2e698b69417
Abikwa <Abikalex8@gmail.com>
2022-03-26 19:48:54 +0200

    make request

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('models');
+var TpData = db.TpData;
+var Sale = db.Sale;
+var Saledetail = db.Saledetail;
+var Article = db.Article;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  
+  var tdData = await TpData.findAll({
+    // attributes: [['category_name', 'dataInvoice']],
+    include : [{
+      model :  Sale,
+      // attributes: [['product_name', 'productInvoiceDetail'], ['unit_price', 'priceInvoiceDetail']]
+      include : [{
+        model : Saledetail,
+        include : {
+          model : Article
+        }
+      }]
+    }]
+  });
+  res.end(JSON.stringify(tdData, null, 2))
+    // console.log(da);
+
   res.render('home/index');
 });
 
