2329c6b716251fc623eaeb5a1e9c30c68c02cbbb
Edgar Eldy <ed.eldy21@live.com>
2022-05-02 22:46:48 +0200

    Add orders/getProducts get route

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ app.post("/customers/delete/:id", customerRouter);
 // Orders endpoints
 app.get("/orders", ordersRouter);
 app.get("/orders/add", ordersRouter);
+app.get('/orders/getProducts/:cat_id', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
