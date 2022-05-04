3b294383fe6f93e60916f3bfe0cd9b07454c8350
Edgar Eldy <ed.eldy21@live.com>
2022-05-04 16:48:12 +0200

    Change to $.ajax() for full configurations

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,19 @@
 $(function () {
     $('#cat_id').on('change', function () {
         var cat_id = $(this).val();
-        $.get('/orders/getProducts', {cat_id: cat_id}, function (data) {
-            $('#product_id').html(data);
+        $.ajax({
+            type: 'GET',
+            async: true,
+            url: '/orders/getProducts/' + cat_id,
+            data: {
+                cat_id: cat_id
+            },
+            success: function (data) {
+                $('#product_id').empty();
+                $.each(data, function (index, row) {
+                    $('#product_id').append("<option value='" + row.id + "'>" + row.product_name + "</option>")
+                });
+            }
         });
     });
 });
