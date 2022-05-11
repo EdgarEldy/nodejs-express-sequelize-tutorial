112ee7f5d576350364abbe332ea119473f40adc8
Abikwa <Abikalex8@gmail.com>
2022-05-11 15:46:03 +0200

    make associate sale to Invoices

--- models/sale.js
+++ models/sale.js
@@ -10,7 +10,13 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Sale.hasMany(models.Saledetail, {
+        as: 'Invoices',
+        foreignKey: 'saleId',
+      });
+      Sale.belongsTo(models.TpData, {
+        foreignKey: 'tpdataId',
+      });
     }
   }
   Sale.init({
