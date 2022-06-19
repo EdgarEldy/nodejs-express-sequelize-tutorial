091258abff1a40c56e83c4c064ce62c14310ae84
Edgar Eldy <ed.eldy21@live.com>
2022-06-19 11:06:57 +0200

    Register orders/id post route

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.get('/orders/getProducts/:cat_id', ordersRouter);
 app.get('/orders/getUnitPrice/:product_id', ordersRouter);
 app.post('/orders', ordersRouter);
 app.get('/orders/edit/:id', ordersRouter);
+app.post('/orders/:id', ordersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
