a645798dbe6de9f687a309721888b599c5b0821c
Edgar Eldy <ed.eldy21@live.com>
2022-06-21 22:55:04 +0200

    Initialize database and Role model

--- routes/roles.js
+++ routes/roles.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+// Initialize database and Role model
+const db = require('../models');
+const Role = db.Role;
+
 module.exports = router;
