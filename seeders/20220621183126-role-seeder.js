c2148705a855d9f37c5cc335d9f059736f31af3e
Edgar Eldy <ed.eldy21@live.com>
2022-06-21 22:46:03 +0200

    Add roles seeder

--- seeders/20220621183126-role-seeder.js
+++ seeders/20220621183126-role-seeder.js
@@ -1,24 +1,36 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        await queryInterface.bulkInsert('roles', [
+            {
+                name: 'Admin',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+            {
+                name: 'User',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        ], {});
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
