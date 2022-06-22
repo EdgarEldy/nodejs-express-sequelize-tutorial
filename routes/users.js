7f77c090299257fc61baa3894724f32f1e05fa21
Edgar Eldy <ed.eldy21@live.com>
2022-06-22 19:29:03 +0200

    Initialize database and User model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// Initialize database and User model
+const db = require('../models');
+const User = db.User;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
