4bce536b9034b3d94f8787a5078383277cb28679
Edgar Eldy <ed.eldy21@live.com>
2022-06-22 19:42:41 +0200

    GET users listing

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,22 @@ var router = express.Router();
 // Initialize database and User model
 const db = require('../models');
 const User = db.User;
+const Role = db.Role;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', async (req, res, next) => {
+    const users = await User.findAll({
+        include: [
+            {
+                model: Role,
+                required: true
+            }
+        ]
+    });
+
+    res.render('users/index', {
+        users: users
+    });
 });
 
 module.exports = router;
